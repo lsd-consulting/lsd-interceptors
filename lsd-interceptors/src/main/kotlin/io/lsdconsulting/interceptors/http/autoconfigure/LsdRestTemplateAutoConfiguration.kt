26105268f3926e98a49b75e53ef04a5ed494274f
Nick McDowall <nicholasinengland@hotmail.com>
2026-09-23 08:59:19 +0100

    fix: adapt example-app and tests for Spring Boot 4
    Rename aop starter to aspectj, move RestTemplateCustomizer and
    HttpMessageConverters to Boot 4 packages, add aspectjweaver and
    spring-boot-restclient, and update Spring Framework 7 test stubs.

--- lsd-interceptors/src/main/kotlin/io/lsdconsulting/interceptors/http/autoconfigure/LsdRestTemplateAutoConfiguration.kt
+++ lsd-interceptors/src/main/kotlin/io/lsdconsulting/interceptors/http/autoconfigure/LsdRestTemplateAutoConfiguration.kt
@@ -6,7 +6,7 @@ import io.lsdconsulting.interceptors.http.LsdRestTemplateInterceptor
 import io.lsdconsulting.interceptors.http.common.HttpInteractionHandler
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.web.client.RestTemplateCustomizer
+import org.springframework.boot.restclient.RestTemplateCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
